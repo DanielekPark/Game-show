3e2a8300b56573c8502f7a07cf56f93d3fd0675c
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2020-01-24 15:58:45 -0800

    js edits

--- js/app.js
+++ js/app.js
@@ -5,21 +5,42 @@ document.addEventListener('DOMContentLoaded', () => {
     const phrase = document.querySelector('#phrase');
     const qwerty = document.querySelector('#qwerty');
     let missed = 0; 
-    const gamePhrase = ['browser', 'web', 'javascript', 'html', 'css'];
+    let i = 0; 
+    const ul = document.querySelector('ul'); 
+    const gamePhrases = [
+    'apple of my eye',
+    'a dime a dozen',
+    'burst your bubble',
+    'cut to the chase',
+    'down to earth'];
 
     
-    //listen for the start game button to be pressed
-    startButton.addEventListener('click', () => {
-        overlay.style.display = 'none'; 
-    }); 
+ 
+/*FUNCTIONS-------------------------------------------------------------*/
 
-    //return a random phrase from an array 
+    //return a random phrase from an array
     const getRandomPhraseAsArray = arr => {
-        let randomNumber = Math.floor(Math.random() * arr.length) + 1;
-        let index = arr.indexOf(randomNumber); 
-        return randomNumber; 
+        const index = Math.floor(Math.random() * arr.length) + 1;
+        const random = arr[index]; 
+        const randomPhrase = random.split(); 
+        return randomPhrase; 
     }
-    getRandomPhraseAsArray(gamePhrase);
+    //const phraseArr = getRandomPhraseAsArray(gamePhrases);
+
+    //adds the letters of a string to the display
+    const addPhraseToDisplay = arr => {
+        for (let i = 0; i < arr.length; i += 1){
+            const li = document.createElement('li');
+            li.appendChild.arr[i]; 
+            ul.appendChild(li);
+            if(arr[i] === arr[i]){
+                arr[i].classList.add('letter');
+            }else if(arr[i] === ' '){
+                arr[i].classList.add('space');
+            } 
+        }
+    }
+ //    const displayLetter = addPhraseToDisplay(phraseArr) FUNCTION CALL
 
     //check if a letter is in the phrase
     const checkLetter = button => {
@@ -33,33 +54,58 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         return match; 
     }
+
+
+
+     //check if the game has been won or lost
+    //  const checkWin = () => {
+    //     const letterClass = document.querySelectorAll('.letter'); 
+    //     const show = document.querySelectorAll('.show');
+    //     const div = document.querySelector('.start');
+    //     const title = document.querySelector('.title');   
+        
+    //     if(letterClass.length === show.length){
+    //         div.classList.add('win');  
+    //         title.textContent = 'YOU WIN!';
+    //         overlay.style.display = 'flex';
+    //     }
+
+    //     if(missed > 4){
+    //         div.classList.add('lose');   
+    //         title.textContent = 'YOU HAVE RUN OUT OF HEARTS PLEASE PLAY AGAIN!';
+    //         overlay.style.display = 'flex';        
+    //     }
+    // } 
+
+/*EVENT HANDLERS--------------------------------------------------------*/
     
+    //listen for the start game button to be pressed
+    startButton.addEventListener('click', () => {
+        overlay.style.display = 'none'; 
+    });
+
     //listen for the onscreen keyboard to be clicked
-  qwerty.addEventListener('click', event => {
-    if(event.target.tagName ==='BUTTON'){
-        event.target.classList.add('chosen');
-        event.target.disabled = true; 
-        const matched = checkLetter(event.target.textContent); 
-        if(!matched){
-            const image = document.querySelectorAll('img');
-
-            let i; 
-            do{
-                i = 0; 
-                i += 1;
+    qwerty.addEventListener('click', event => {
+        if(event.target.tagName ==='BUTTON'){
+            event.target.classList.add('chosen');
+            event.target.disabled = true; 
+            const matched = checkLetter(event.target.textContent); 
+            if(!matched){
+                const image = document.querySelectorAll('img');
+                image[i].src = 'images/lostHeart.png';
+                i ++;
                 missed += 1;
-            }while(i < 1)  
-            image[i].src = 'images/lostHeart.png'; 
+            }
+            //checkWin();         
         }
-    }
-  }); 
-
-    
-   
-   //adds the letters of a string to the display
-
-    //check if the game has been won or lost
-    
+    }); 
  
-}); 
+
+
+ 
+});
+
+
+
+
 
